40f4f805b2f94492be5a1b3663a3db1d25b1b3a9
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-03-05 16:18:02 +0400

    remove read news support

--- android/mobius_rss/src/main/java/com/intellij/mobiusrss/FeedActivity.java
+++ android/mobius_rss/src/main/java/com/intellij/mobiusrss/FeedActivity.java
@@ -3,10 +3,7 @@ package com.intellij.mobiusrss;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,24 +13,14 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-
 import java.text.DateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import nl.matshofman.saxrssreader.RssFeed;
 import nl.matshofman.saxrssreader.RssItem;
 
 public class FeedActivity extends Activity {
-  private static final String READ_NEWS_KEY = "read_news";
-  private static final String LOG_TAG = FeedActivity.class.getName();
-
   public static final String URL_EXTRA = "URL";
 
   private ProgressBar myProgressBar;
@@ -53,15 +40,6 @@ public class FeedActivity extends Activity {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         final RssItem item = myListAdapter.getItem(position);
-        myListAdapter.myReadSet.add(item.getPubDate());
-        myListAdapter.notifyDataSetChanged();
-        try {
-          final String s = saveReadNewsSet(myListAdapter.myReadSet);
-          final SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
-          prefs.edit().putString(READ_NEWS_KEY, s).commit();
-        } catch (JSONException e) {
-          Log.e(LOG_TAG, "", e);
-        }
         startActivity(NoteActivity.createIntent(FeedActivity.this, item));
       }
     });
@@ -79,26 +57,6 @@ public class FeedActivity extends Activity {
     new MyRssFeedLoadingTask(url).execute();
   }
 
-  private static Set<Date> loadReadNewsSet(String s) throws JSONException {
-    final JSONArray jsonArray = new JSONArray(s);
-    final int size = jsonArray.length();
-    final Set<Date> result = new HashSet<Date>(size);
-
-    for (int i = 0; i < size; i++) {
-      result.add(new Date(jsonArray.getLong(i)));
-    }
-    return result;
-  }
-
-  private static String saveReadNewsSet(Set<Date> set) throws JSONException {
-    final List<Long> longs = new ArrayList<Long>(set.size());
-
-    for (Date date : set) {
-      longs.add(date.getTime());
-    }
-    return new JSONArray(longs).toString();
-  }
-
   @SuppressWarnings("ConstantConditions")
   private void showRssFeed(RssFeed rssFeed) {
     final String title = rssFeed.getTitle();
@@ -106,18 +64,7 @@ public class FeedActivity extends Activity {
     if (title != null && !title.isEmpty()) {
       setTitle(title);
     }
-    final SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
-    final String readNewsStr = prefs.getString(READ_NEWS_KEY, "");
-    Set<Date> readSet = Collections.emptySet();
-
-    if (!readNewsStr.isEmpty()) {
-      try {
-        readSet = loadReadNewsSet(readNewsStr);
-      } catch (JSONException e) {
-        Log.e(LOG_TAG, "", e);
-      }
-    }
-    myListAdapter = new MyListAdapter(rssFeed.getRssItems(), readSet);
+    myListAdapter = new MyListAdapter(rssFeed.getRssItems());
     myListView.setAdapter(myListAdapter);
   }
 
@@ -143,11 +90,9 @@ public class FeedActivity extends Activity {
 
   private class MyListAdapter extends BaseAdapter {
     private final List<RssItem> myItems;
-    private final Set<Date> myReadSet;
 
-    private MyListAdapter(List<RssItem> items, Set<Date> readSet) {
+    private MyListAdapter(List<RssItem> items) {
       myItems = items;
-      myReadSet = new HashSet<Date>(readSet);
     }
 
     @Override
@@ -174,7 +119,6 @@ public class FeedActivity extends Activity {
           R.layout.rss_item, parent, false);
       final TextView titleView = (TextView) view.findViewById(R.id.rssItemTitleView);
       titleView.setText(rssItem.getTitle());
-      titleView.setTypeface(myReadSet.contains(date) ? Typeface.DEFAULT : Typeface.DEFAULT_BOLD);
 
       final TextView dateView = (TextView) view.findViewById(R.id.rssItemDateView);
       dateView.setText(DateFormat.getDateInstance().format(date));
