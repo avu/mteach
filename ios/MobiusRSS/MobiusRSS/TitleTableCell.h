065f9700cc3585f568e61accb64f6d8be89001db
Alexey Ushakov <Alexey.Ushakov@jetbrains.com>
2014-03-11 19:18:44 +0400

    Added missing file

---
+++ ios/MobiusRSS/MobiusRSS/TitleTableCell.h
@@ -0,0 +1,15 @@
+//
+//  TitleTableCell.h
+//  MobiusRSS
+//
+//  Created by Alexey Ushakov on 3/10/14.
+//  Copyright (c) 2014 jetbrains. All rights reserved.
+//
+
+
+
+@interface TitleTableCell : UITableViewCell {
+}
+@property IBOutlet UILabel *title;
+@property IBOutlet UILabel *date;
+@end
